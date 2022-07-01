ed5c7313a1b0e20e08be5b54eda2b7c4f0078f7d
Brice Veyna <bricev99@gmail.com>
2022-07-01 09:23:27 -0700

    Add for loop, modify variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,34 +2,35 @@
 var today = moment();
 $('#currentDay').text(today.format("dddd, MMMM Do YYYY"));
 
-var currentTime = today.format("hh A");
+// Store current time to track for style class
+var currentTime = today.format("h");
 console.log(currentTime);
 
-var rowOne = moment('09', 'kk').format('hh A');
-var rowTwo = moment('10', 'kk').format('hh A');
-var rowThree = moment('11', 'kk').format('hh A');
-var rowFour = moment('12', 'kk').format('hh A');
-var rowFive = moment('13', 'kk').format('hh A');
-var rowSix = moment('14', 'kk').format('hh A');
-var rowSeven = moment('15', 'kk').format('hh A');
-var rowEight = moment('16', 'kk').format('hh A');
-var rowNine = moment('17', 'kk').format('hh A');
-console.log(rowNine);
+// Store variable for standard business hours
+var businessHours24 = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 
-var timeBlockEl = $('.time-block');
+for (var i = 0; i < businessHours24.length; i++) {
 
+    var timeBlockEl = $('.description');
+    var businessHours = moment(businessHours24, "k").format("h");
 
-
-if (currentTime < i) {
-    timeBlockEl.removeclass("past");
-    timeBlockEl.removeclass("present");
-    timeBlockEl.attr('class', 'future');
-} else if (currentTime > i) {
-    timeBlockEl.attr('class', 'past');
-    timeBlockEl.removeclass("present");
-    timeBlockEl.removeclass("future");
-} else {
-    timeBlockEl.removeclass("past");
-    timeBlockEl.attr('class', 'present');
-    timeBlockEl.removeclass("present");
+    // Conditional statement, attaches style class to row depending on current time
+    if (currentTime < businessHours[i]) {
+        console.log(businessHours[i]);
+        timeBlockEl.removeClass("past");
+        timeBlockEl.removeClass("present");
+        timeBlockEl.addClass("future");
+    } else if (currentTime > businessHours[i]) {
+        console.log(businessHours[i]);
+        timeBlockEl.addClass("past");
+        timeBlockEl.removeClass("present");
+        timeBlockEl.removeClass("future");
+    } else {
+        console.log(businessHours[i]);
+        timeBlockEl.removeClass("past");
+        timeBlockEl.addClass("present");
+        timeBlockEl.removeClass("present");
+    }
 }
+
+// Save button stores text in local storage
